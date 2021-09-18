23546f03c39c594271bd85d2052e4efc6faef551
Sarthak Tamhankar <74110205+SuperS-1@users.noreply.github.com>
2021-09-18 13:55:25 +0530

    Update googledemo.java

--- src/test/java/com/seleniumtest/seleniumtest/googledemo.java
+++ src/test/java/com/seleniumtest/seleniumtest/googledemo.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
@@ -12,18 +12,30 @@ import org.testng.annotations.AfterTest;
 		private WebDriver driver;
 
 		@Test(priority=0) 
-		public void testJbkTitle() {
+		public void test1() {
 		    driver.get("http://www.google.com"); 
 		    String title = driver.getTitle(); 
 		    System.out.println(title);
 		    Assert.assertEquals(title,"Google");
 		   
 		}
+		
+		@Test
+		 public void test2() {
+		    WebElement gmail = driver.findElement(By.className("gb_f"));
+		    gmail.click();
+		}
+
+		@Test
+		public void test3() {
+		    WebElement createaccount = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/span"));
+		    createaccount.click();
+		}
 
 			@BeforeTest
 			public void beforeTest() {
-				System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
-			 driver = new ChromeDriver();
+				System.setProperty("webdriver.edge.driver","C:\\edgedriver_win64\\msedgedriver.exe");
+			 driver = new EdgeDriver();
 			}
 
 			@AfterTest
